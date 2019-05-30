3dfc16af60d20a71b837f61338e6171189f8bc39
alumno informatica <alumno@ciis.usil.edu.pe>
2019-05-30 16:46:35 -0500

    primer avance

---
+++ politicas-de-envio.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Politicas de envio | Pisquerito</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="css/pg.css" />
+
+	<link rel="shortcut icon" type="image/x-icon" href="img\ico.png" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+	<link href='https://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<?php include 'partes/header.php' ?>
+    <?php include 'partes/navegacion.php' ?>
+    <div id="cont">
+
+    
+    <h1>Politicas de envio</h1>
+
+    <div>
+        <h2>The standard Lorem Ipsum passage, used since the 1500s</h2>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+   </div>
+   
+   </div>
+
+
+</body>
+</html>
