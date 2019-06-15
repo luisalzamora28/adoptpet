4269c5aaa60a08a5f078d644eee9c0e2668fefd6
alumno informatica <alumno@ciis.usil.edu.pe>
2019-06-15 12:52:30 -0500

    Agrergar estilos

--- index.php
+++ index.php
@@ -1,42 +1,20 @@
-<?php
-session_start();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inicio | PETADOPT</title>
-    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+<?php include 'plantillas/header.php' ?>
 
-    
+<h1>Inicio</h1>
+<div>
+    <h3></h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+</div>
+<div>
+    <h3></h3>
+    <p>Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with other websites. Lorem Ipsum best not make any more threats to your website. It will be met with fire and fury like the world has never seen. Does everybody know that pig named Lorem Ipsum? An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud.</p>
+</div>
+<div>
+    <h3></h3>
+    <p>Lorem ipsum dolor amet mustache knausgaard +1, blue bottle waistcoat tbh semiotics artisan synth stumptown gastropub cornhole celiac swag. Brunch raclette vexillologist post-ironic glossier ennui XOXO mlkshk godard pour-over blog tumblr humblebrag. Blue bottle put a bird on it twee prism biodiesel brooklyn. Blue bottle ennui tbh succulents.</p>
+</div>
+<div>
+    <a href="adopta.php">Ver mascotas</a>
+</div>
 
-</head>
-<body>
-<?php include 'partes/navegar.php' ?>
-    
-    
-    <h1>Inicio</h1>
-    <div  >
-            <h3></h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-</p>
-    </div>
-    <div  >
-            <h3></h3>
-            <p>Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with other websites. Lorem Ipsum best not make any more threats to your website. It will be met with fire and fury like the world has never seen. Does everybody know that pig named Lorem Ipsum? An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud.
-</p>
-    </div>
-    <div >
-            <h3></h3>
-            <p>Lorem ipsum dolor amet mustache knausgaard +1, blue bottle waistcoat tbh semiotics artisan synth stumptown gastropub cornhole celiac swag. Brunch raclette vexillologist post-ironic glossier ennui XOXO mlkshk godard pour-over blog tumblr humblebrag. Blue bottle put a bird on it twee prism biodiesel brooklyn. Blue bottle ennui tbh succulents.
-</p>
-    </div>
-    <div>
-        <button> <a href="adopta.php">Ver mascotas</a> </button>
-    </div>
-    
- 
-</body>
-</html>
+<?php include 'plantillas/footer.php' ?>
---
+++ plantillas/footer.php
@@ -0,0 +1,6 @@
+	</div></section>
+	<footer><div class="wrapper">
+		<div style="text-align:center;padding:10px 0px;">Todos los derechos reservados</div>
+	</div></footer>
+</body>
+</html>
